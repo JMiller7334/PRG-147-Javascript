e7af4ee69f92bc9e821354fb70d7a819582a9939
JMiller7334 <42582030+JMiller7334@users.noreply.github.com>
2022-02-27 00:28:33 -0600

    Update myLoop.js

--- Module2/assessment05/scripts/myLoop.js
+++ Module2/assessment05/scripts/myLoop.js
@@ -5,23 +5,23 @@ class month {
     }
 }
 
-let jan =  new month("January", ["New Years Day", "Martin Luther King Jr. Day"])
-let feb = new month("February", ["Ground Hog Day", "Valentine's Day"])
-let mar = new month("March", ["Saint Patrick's Day", "Day Light Savings", "Marty Gros"])
-let apr = new month("April", ["April", "April Fools Day", "Easter"])
-let may = new month("May", ["Mothers Day", "Memorial Day"])
-let jun = new month("June", ["Flag Day", "Fathers Day", "Global Running Day"])
-let jul = new month("July", ["Independance Day", "Parents Day"])
-let aug = new month("August", ["Cheap Flight Day", "National Waffle Day"])
-let sep = new month("September", ["Labour Day", "Citizenship Day"])
-let oct = new month("October", ["Sweetest Day", "Holloween"])
-let nov = new month("November", ["Thanksgiving", "Black Friday"])
-let dec = new month("December",["Christmas", "New Years Eve"])
+let jan =  new month("January", ["New Years Day", "Martin Luther King Jr. Day"]);
+let feb = new month("February", ["Ground Hog Day", "Valentine's Day"]);
+let mar = new month("March", ["Saint Patrick's Day", "Day Light Savings", "Marty Gros"]);
+let apr = new month("April", ["April", "April Fools Day", "Easter"]);
+let may = new month("May", ["Mothers Day", "Memorial Day"]);
+let jun = new month("June", ["Flag Day", "Fathers Day", "Global Running Day"]);
+let jul = new month("July", ["Independance Day", "Parents Day"]);
+let aug = new month("August", ["Cheap Flight Day", "National Waffle Day"]);
+let sep = new month("September", ["Labour Day", "Citizenship Day"]);
+let oct = new month("October", ["Sweetest Day", "Holloween"]);
+let nov = new month("November", ["Thanksgiving", "Black Friday"]);
+let dec = new month("December",["Christmas", "New Years Eve"]);
 
-const fullYear = [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec]
-let count = 0
+const fullYear = [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec];
+let count = 0;
 for (i in fullYear) {
-    count ++
+    count ++;
     //document.getElementById(count+"month").innerHTML = fullYear[i].name;
    //document.getElementById(count+"day").innerHTML = fullYear[i].holidays
 
